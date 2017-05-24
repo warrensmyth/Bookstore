bd3d79fb451f31d6ca88d749871065bd02669bdc
Warren Smyth <warrensmyth@me.com>
2017-05-24 14:40:25 -0700

    Iteration J1: Selecting the Locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-
   controller :sessions do
     get    'login'  => :new
     post   'login'  => :create
@@ -8,13 +7,16 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-
   resources :products do
   	get :who_bought, on:  :member
   end
+  
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via:  :all
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
